d1402e84ec727a242ed34ea2f4dc8aea0579ab30
Jithin Pradeep <jithinpr2@gmail.com>
2016-11-21 00:42:47 -0600

    Update costFunction.m

--- MATLAB Implementation/Linear regression/costFunction.m
+++ MATLAB Implementation/Linear regression/costFunction.m
@@ -1,6 +1,6 @@
 function J = costFunction(X, y, theta)
-%COMPUTECOSTMULTI Compute cost function J(theta) for linear regression 
-% USAGE :  J = COMPUTECOSTMULTI(X, y, theta) 
+%COMPUTECOST Compute cost function J(theta) for linear regression 
+% USAGE :  J = costFunction(X, y, theta) 
 
 m = length(y); % number of training examples
 J = (1/(2*m))*sum((X*theta - y).^2);
